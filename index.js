643c7bb829a5e0d4d0330d99259764f298af7322
xuedy <xuedy@ctrip.com>
2017-02-27 15:16:34 +0800

    rename

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 var express = require('express');
 var app = express();
 
-var __TIME_MARK_DATE = [];
+var __TIME_MARK_DEBUG = [];
 app.get('/log', function(req, res) {
 	var logName = req.query.key;
-	__TIME_MARK_DATE.push({
-		id : logName || __TIME_MARK_DATE.length,
+	__TIME_MARK_DEBUG.push({
+		id : logName || __TIME_MARK_DEBUG.length,
 		time : +new Date()
 	});
 	res.end();
 });
 app.get('/clean', function(req, res) {
-	__TIME_MARK_DATE = [];
+	__TIME_MARK_DEBUG = [];
 	res.end('clean ok!');
 });
 app.get('/report', function(req, res) {
 	var report = [];
-	for (var i = 1; i < __TIME_MARK_DATE.length; i++) {
+	for (var i = 1; i < __TIME_MARK_DEBUG.length; i++) {
 		report
 				.push('"'
-						+ __TIME_MARK_DATE[i].id
+						+ __TIME_MARK_DEBUG[i].id
 						+ '"'
 						+ '减去'
 						+ '"'
-						+ __TIME_MARK_DATE[i - 1].id
+						+ __TIME_MARK_DEBUG[i - 1].id
 						+ '"'
 						+ '等于'
 						+ '"'
-						+ (__TIME_MARK_DATE[i].time
-								- __TIME_MARK_DATE[i - 1].time + '"'))
+						+ (__TIME_MARK_DEBUG[i].time
+								- __TIME_MARK_DEBUG[i - 1].time + '"'))
 	}
-	res.end(JSON.stringify(__TIME_MARK_DATE) + '\r\n' + report.join('\r\n'));
+	res.end(JSON.stringify(__TIME_MARK_DEBUG) + '\r\n' + report.join('\r\n'));
 });
 
 var server = app.listen(5387, function() {
