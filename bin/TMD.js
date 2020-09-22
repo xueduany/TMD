b838d273c7b0bd79436ad658976070b103bfe6d6
xueduanyang <xueduanyang@bytedance.com>
2020-09-22 18:28:39 +0800

    fix bug

--- bin/TMD.js
+++ bin/TMD.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
 const exec = require("child_process").exec;
-const args = process.argv.slice(1);
+const args = process.argv.slice(2);
 exec('node '+__dirname+'/../index.js '+args.join(' ')).stdout.pipe(process.stdout);
