1f40949fa205bf476a438a218dbc4e682cc80ede
xueduanyang <xueduanyang@bytedance.com>
2020-09-22 18:11:22 +0800

    sync

--- bin/TMD.js
+++ bin/TMD.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 const exec = require("child_process").exec;
-exec('node ../index.js').stdout.pipe(process.stdout);
+exec('node '+__dirname+'/../index.js').stdout.pipe(process.stdout);
